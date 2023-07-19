468d693eb31b9825409732b20dadcdc096aacb48
hafiz98m <hafizm35@gmail.com>
2023-07-19 20:16:17 +0700

    work

--- js/index.js
+++ js/index.js
@@ -4,6 +4,21 @@ form.addEventListener("submit", getValues);
 
 function getValues(event) {
   event.preventDefault();
-  let jenisKelamin = this.jenis - kelamin.value;
+
+  const jenisKelamin = this.jenisKelamin.value;
   console.log(jenisKelamin);
+  const berat = this.beratBadan.value;
+  //   console.log(berat);
+  const usia = this.usia.value;
+  //   console.log(usia);
+  const tinggi = this.tinggiBadan.value / 100;
+  //   console.log(tinggi);
+
+  let bmi = berat / (tinggi ^ 2);
+  console.log(bmi);
+
+  let out = `<p>BMI adalah ${bmi}</p>
+  `;
+
+  document.querySelector(".hasil").innerHTML = out;
 }
